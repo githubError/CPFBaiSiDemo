8b0f6114c2c050dcd0e1ddd8f96aacba4cc7b359
崔鹏飞 <cpf9401@163.com>
2016-06-15 16:25:13 +0000

    TodayExtension

---
+++ 百思不得姐/TodayExtenssion/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  TodayExtenssion
+//
+//  Created by cuipengfei on 16/6/15.
+//  Copyright © 2016年 崔鹏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ 百思不得姐/TodayExtenssion/TodayViewController.m
@@ -0,0 +1,43 @@
+//
+//  TodayViewController.m
+//  TodayExtenssion
+//
+//  Created by cuipengfei on 16/6/15.
+//  Copyright © 2016年 崔鹏飞. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets {
+    
+    return UIEdgeInsetsMake(defaultMarginInsets.top, defaultMarginInsets.left - 48, defaultMarginInsets.bottom, defaultMarginInsets.right);
+}
+
+@end
