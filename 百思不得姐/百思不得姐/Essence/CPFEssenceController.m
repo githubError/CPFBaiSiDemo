c41b2a26a0b8123a1c7f90c901edbb9c30b5af8d
崔鹏飞 <cpf9401@163.com>
2016-07-08 17:50:38 +0000

    添加标签栏默认选中

--- 百思不得姐/百思不得姐/Essence/CPFEssenceController.m
+++ 百思不得姐/百思不得姐/Essence/CPFEssenceController.m
@@ -29,6 +29,7 @@
 
 // 设置标签栏
 - (void)setupTitlesView {
+    // 标签栏背景
     UIView *titlesView = [[UIView alloc] init];
     titlesView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.7];
     titlesView.width = self.view.width;
@@ -38,6 +39,15 @@
     NSArray *titles = @[@"全部",@"图片",@"语音",@"视频",@"段子"];
     CGFloat width = titlesView.width / titles.count;
     CGFloat height = titlesView.height;
+    
+    // 标签栏按钮指示器
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.height = 2;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    indicatorView.backgroundColor = [UIColor redColor];
+    [titlesView addSubview:indicatorView];
+    
+    self.indicatorView = indicatorView;
     
     for (NSInteger i = 0; i < titles.count; i++) {
         UIButton *button = [[UIButton alloc] init];
@@ -52,15 +62,17 @@
         
         [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
         [titlesView addSubview:button];
+        
+        // 默认选中
+        if (i == 0) {
+            button.enabled = NO;
+            self.selectedBtn = button;
+            [button.titleLabel sizeToFit];
+            self.indicatorView.width = button.titleLabel.width;
+            self.indicatorView.centerX = button.centerX;
+        }
     }
     
-    UIView *indicatorView = [[UIView alloc] init];
-    indicatorView.height = 2;
-    indicatorView.y = titlesView.height - indicatorView.height;
-    indicatorView.backgroundColor = [UIColor redColor];
-    [titlesView addSubview:indicatorView];
-    
-    self.indicatorView = indicatorView;
     
     [self.view addSubview:titlesView];
 }
