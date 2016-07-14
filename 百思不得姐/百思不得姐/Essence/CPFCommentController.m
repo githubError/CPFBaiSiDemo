6553327159167f812195f4d8069aa29e4a9c0076
崔鹏飞 <cpf9401@163.com>
2016-07-14 21:24:19 +0000

    显示topicCell

--- 百思不得姐/百思不得姐/Essence/CPFCommentController.m
+++ 百思不得姐/百思不得姐/Essence/CPFCommentController.m
@@ -8,7 +8,7 @@
 
 #import "CPFCommentController.h"
 
-@interface CPFCommentController () <UITableViewDelegate>
+@interface CPFCommentController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomSpace;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -19,12 +19,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self setupNavgationItem];
     
+    // 设置导航栏
+    [self setupNavgationItem];
+    // 设置tableHeaderView
+    [self setupTableHeaderView];
+}
+
+- (void)setupTableHeaderView {
+    UIView *header = [[UIView alloc] init];
     CPFTopicCell *topicCell = [CPFTopicCell topicCell];
     topicCell.topic = self.topic;
-    topicCell.height = topicCell.topic.cellHeight;
-    self.tableView.tableHeaderView = topicCell;
+    topicCell.size = CGSizeMake(CPFScreenW, self.topic.cellHeight);
+    [header addSubview:topicCell];
+    header.height = topicCell.topic.cellHeight + CPFTopicCellMargin;
+    self.tableView.tableHeaderView = header;
 }
 
 - (void)setupNavgationItem {
@@ -32,6 +41,8 @@
     self.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithImage:@"comment_nav_item_share_icon" highImage:@"comment_nav_item_share_icon_click" target:nil action:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    
+    self.view.backgroundColor = CPFGlobalBg;
 }
 
 - (void)keyboardWillChangeFrame:(NSNotification *)notification {
@@ -45,9 +56,46 @@
     }];
 }
 
+#pragma mark - tableView数据源
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 10;
+    } else {
+        return 20;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return @"热门评论";
+    } else {
+        return @"评论";
+    }
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"comment"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"comment"];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%zd----%zd",indexPath.section, indexPath.row];
+    return cell;
+}
+
+#pragma mark - tableView代理方法
+
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
     [self.view endEditing:YES];
 }
+
+
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
